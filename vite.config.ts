0117b85b86315408ee992ba7b46030045366b1eb
Abbie <aitong0113@gmail.com>
2025-12-03 21:09:07 +0800

    修正GitHub Pages白畫面

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TS-task2/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
